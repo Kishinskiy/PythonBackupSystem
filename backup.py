30d9f28206f2e9ec8d46e91a9172c158a1f9a367
Oleg Kishinskiy <oleg@kishinskiy.ru>
2019-08-12 15:14:42 +0300

    move backup file to destination folder

--- backup.py
+++ backup.py
@@ -10,7 +10,7 @@ import zipfile
 import shutil
 
 
-# MAX = 500*1024*1024    # 500Mb    - max chapter size
+# # MAX = 500*1024*1024    # 500Mb    - max chapter size
 MAX = 500*1024*1024
 BUF = 50*1024*1024*1024    # 50GB     - memory buffer size
 
@@ -21,6 +21,8 @@ def parse_input():
     parser.add_argument('-src', nargs='*', type=str, required=True, help='Path to Source folder')
     parser.add_argument('-dst', nargs='*', type=str, required=True, help='Path to Destination folder')
     parser.add_argument('-name', nargs='*', type=str, required=True, help='Archive name .zip')
+    # parser.add_argument('-s', nargs='?', const=1, type=int, default=500)
+    # parser.add_argument('-b', nargs='?', const=1, type=int, default=50)
     # no input means show me the help
     if len(sys.argv) == 0:
         parser.print_help()
@@ -33,6 +35,9 @@ def file_split(FILE, MAX):
     '''Split file into pieces, every size is  MAX = 15*1024*1024 Byte'''
     chapters = 1
     uglybuf = ''
+
+
+
     with open(FILE, 'rb') as src:
         while True:
             tgt = open(FILE + '.%03d' % chapters, 'wb')
@@ -68,7 +73,8 @@ def docopy(source_folder, target_folder):
     for subdir, dirs, files in os.walk(source_folder):
         for file in files:
             print(os.path.join(subdir, file))
-            shutil.copy2(os.path.join(subdir, file), target_folder)
+            # shutil.copy2(os.path.join(subdir, file), target_folder)
+            shutil.move(os.path.join(subdir, file), target_folder)
 
 
 if __name__ == '__main__':
@@ -78,6 +84,17 @@ if __name__ == '__main__':
     dest = arg.dst[0]
     source = arg.src[0]
     name = arg.name[0]
+    # size = arg.s[0]
+    # buf = arg.b[0]
+    #
+    # MAX = size * 1024 * 1024
+    # BUF = buf * 1024 * 1024 * 1024
+
+    path = 'Backup'
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    os.chdir(path)
 
     # compress to zip
     doprocess(source, name)
@@ -85,7 +102,15 @@ if __name__ == '__main__':
     # split zip archive
     file_split(name, MAX)
 
+    os.remove(name)
+
+    os.chdir("..")
+
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+
+
     # copy to backup folder
-    # docopy(source, dest)
+    docopy(path, dest)
 
     print('Ending execution')
